97a09e5ab00d468ed880353f17a88fbc51fb0783
wjdqls62 <wjdqls62@naver.com>
2021-01-27 10:32:12 +0900

    * isDebug 변수 추가
    * 프로토콜, URL 변수 분리

--- src/com/jiran/qa/Common/Config.java
+++ src/com/jiran/qa/Common/Config.java
@@ -1,7 +1,10 @@
 package com.jiran.qa.Common;
 
 public class Config {
-    public static final String REQUEST_MEDIA_URL = "http://10.53.168.125:7070/wp-json/wp/v2/media/";
-    public static final String REQUEST_POST_URL = "http://10.53.168.125:7070/wp-json/wp/v2/posts/";
-    public static final String REQUEST_CATEGORIES_URL = "http://10.53.168.125:7070/wp-json/wp/v2/categories/";
+    public static final Boolean isDebug = true;
+    public static final String BASE_PROTOCOL = "http://";
+    public static final String BASE_URL = "10.53.168.125:7070";
+    public static final String REQUEST_MEDIA_URL = BASE_PROTOCOL + BASE_URL + "/wp-json/wp/v2/media/";
+    public static final String REQUEST_POST_URL = BASE_PROTOCOL + BASE_URL + "/wp-json/wp/v2/posts/";
+    public static final String REQUEST_CATEGORIES_URL = BASE_PROTOCOL + BASE_URL + "/wp-json/wp/v2/categories/";
 }
