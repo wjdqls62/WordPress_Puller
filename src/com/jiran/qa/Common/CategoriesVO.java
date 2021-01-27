09d705b3f9fc5601fe2c87ccbce2a70a7a19e8af
wjdqls62 <wjdqls62@naver.com>
2021-01-27 10:33:54 +0900

    * CategoriesVO 클래스 추가   카테고리 정보를 갖고있는 클래스이며,   JList에서 include, exclude 카테고리와 연관

--- src/com/jiran/qa/Common/CategoriesVO.java
+++ src/com/jiran/qa/Common/CategoriesVO.java
@@ -1,9 +1,39 @@
 package com.jiran.qa.Common;
 
+import org.json.JSONObject;
+
 public class CategoriesVO {
-    public String Categories_id;
-    public String Categories_name;
-    public String Categories_slug;
-    public String Categories_link;
-    public String Categories_desc;
+    private String Categories_id;
+    private String Categories_name;
+    private String Categories_slug;
+    private String Categories_link;
+    private String Categories_desc;
+
+    public void setCategories(JSONObject object){
+        Categories_id = object.get("id").toString();
+        Categories_name = object.get("name").toString();
+        Categories_slug = object.get("slug").toString();
+        Categories_link = object.get("link").toString();
+        Categories_desc = object.get("description").toString();
+    }
+
+    public String getCategories_id(){
+        return Categories_id;
+    }
+
+    public String getCategories_name(){
+        return Categories_name;
+    }
+
+    public String getCategories_slug(){
+        return Categories_slug;
+    }
+
+    public String getCategories_link(){
+        return Categories_link;
+    }
+
+    public String getCategories_desc(){
+        return Categories_desc;
+    }
 }
